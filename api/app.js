293dd68e48fb472df9b82791f4e4c2b6483dc9c3
Pankaj Sherchan <pankaj.sherchan@selu.edu>
2019-06-13 17:19:58 -0400

    added grahql sales api endpoint

--- api/app.js
+++ api/app.js
@@ -5,16 +5,33 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const sales = [];
+
+
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Sale {
+            _id: ID!
+            product: String!
+            customer: String!
+            total: Float!
+            date: String!
+        }
+
+        input SaleInput {
+            product: String!
+            customer: String!
+            total: Float!
+        }
+
         type RootQuery {
-            sales: [String!]!
+            sales: [Sale!]!
         }
 
         type RootMutation {
-            createSale(name: String): String
+            createSale(saleInput: SaleInput): Sale
         }
 
         schema {
@@ -24,11 +41,18 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         sales: () => {
-            return ['Sale1', 'Sale2', 'Sale3']
+            return sales;
         },
         createSale: (args) => {
-            const saleName = args.name;
-            return saleName;
+            const sale = {
+                _id: Math.random().toString(),
+                product: args.saleInput.product,
+                customer: args.saleInput.customer,
+                total: +args.saleInput.total,
+                date: new Date().toISOString()
+            };
+            sales.push(sale);
+            return sale;
         }
     },
     graphiql: true
